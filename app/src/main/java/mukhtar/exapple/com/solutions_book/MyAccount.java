2c065bfbcf7548843702e414df8b3d035f3bc929
Akhmediyar <akhmediyar.sdu@mail.ru>
2016-11-20 01:04:36 +0600

    Ali tolyk emes

--- app/src/main/java/mukhtar/exapple/com/solutions_book/MyAccount.java
+++ app/src/main/java/mukhtar/exapple/com/solutions_book/MyAccount.java
@@ -1,5 +1,6 @@
 package mukhtar.exapple.com.solutions_book;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,6 +27,12 @@ public class MyAccount extends AppCompatActivity {
     EditText etSurname;
     EditText etPas1;
     EditText etPas2;
+    ImageButton changeName;
+    ImageButton changeSurname;
+    ImageButton changePas;
+    Button save;
+    Button del;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +43,8 @@ public class MyAccount extends AppCompatActivity {
         etSurname = (EditText) findViewById(R.id.surnametext);
         etPas1 = (EditText) findViewById(R.id.changetext1);
         etPas2 = (EditText) findViewById(R.id.changetext2);
+        save = (Button) findViewById(R.id.save);
+        del = (Button) findViewById(R.id.delete);
         sharedPref = getSharedPreferences("Username",this.MODE_PRIVATE);
         etName.setText(sharedPref.getString("name",""));
         etSurname.setText(sharedPref.getString("surname",""));
@@ -42,8 +52,17 @@ public class MyAccount extends AppCompatActivity {
         etSurname.setEnabled(false);
         etPas1.setVisibility(View.INVISIBLE);
         etPas2.setVisibility(View.INVISIBLE);
+        changeName = (ImageButton) findViewById(R.id.changeName);
+        changeSurname = (ImageButton) findViewById(R.id.changeSurname);
+        changePas = (ImageButton) findViewById(R.id.changePas);
+        changeName.setOnClickListener(onClickListener);
+        changeSurname.setOnClickListener(onClickListener);
+        changePas.setOnClickListener(onClickListener);
+        save.setOnClickListener(onClickListener);
+        del.setOnClickListener(onClickListener);
 
     }
+
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
 
@@ -60,7 +79,46 @@ public class MyAccount extends AppCompatActivity {
                     etPas1.setVisibility(View.VISIBLE);
                     etPas2.setVisibility(View.VISIBLE);
                     break;
+                case R.id.save:
+                    if(!etName.getText().toString().equals(sharedPref.getString("name",""))){
+                        SharedPreferences.Editor ed = sharedPref.edit();
+                        ContentValues cv = new ContentValues();
+                        cv.put("name",etName.getText().toString());
+                        db.update("information",cv,"name=?",new String[]{sharedPref.getString("name","")});
+                        cv.clear();
+                        ed.putString("name",etName.getText().toString());
+                        ed.commit();
+                    }
+                    if(!etSurname.getText().toString().equals(sharedPref.getString("surname",""))){
+                        SharedPreferences.Editor ed = sharedPref.edit();
+                        ContentValues cv = new ContentValues();
+                        cv.put("name",etSurname.getText().toString());
+                        db.update("information",cv,"surname=?",new String[]{sharedPref.getString("surname","")});
+                        cv.clear();
+                        ed.putString("surname",etSurname.getText().toString());
+                        ed.commit();
+                    }
+                    if(etPas1.getText().toString().equals(etPas2.getText().toString())&&!etPas1.getText().toString().equals(sharedPref.getString("password",""))){
+                        SharedPreferences.Editor ed = sharedPref.edit();
+                        ContentValues cv = new ContentValues();
+                        cv.put("name",etPas1.getText().toString());
+                        db.update("users",cv,"password=?",new String[]{sharedPref.getString("password","")});
+                        cv.clear();
+                        ed.putString("surname",etPas1.getText().toString());
+                        ed.commit();
+                        db.close();
+                    }
+                    break;
+                case R.id.delete:
+                    SharedPreferences.Editor ed = sharedPref.edit();
 
+                    db.delete("users","login=?",new String[]{sharedPref.getString("username","")});
+                    ed.putString("username","");
+                    ed.commit();
+                    db.close();
+                    Intent intent = new Intent(getBaseContext(),Login.class);
+                    startActivity(intent);
+                    finish();
 
             }
         }
