0c939513fc5c162c3b2f8b4207a12c2940b51653
Mukhtar Bimurat <mukhtar.bimurat@ce.sdu.edu.kz>
2016-12-09 12:14:42 +0600

    changed categories

--- app/src/main/java/mukhtar/exapple/com/solutions_book/BooksAppereance/BooksResult.java
+++ app/src/main/java/mukhtar/exapple/com/solutions_book/BooksAppereance/BooksResult.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -24,12 +25,13 @@ public class BooksResult extends AppCompatActivity {
         setContentView(R.layout.activity_books_result);
         String query = getIntent().getStringExtra("id_of_category");
         ListView lv = (ListView) findViewById(R.id.book_result_lv);
-        if (query == "search") {
+        LinearLayout ll = (LinearLayout) findViewById(R.id.activity_books_result);
+        if (query.equals("search")) {
             LayoutInflater inflater = (this).getLayoutInflater();
             View view = inflater.inflate(R.layout.item_search, null, false);
             search = (EditText) view.findViewById(R.id.edittext_search_book);
             button_search = (Button) view.findViewById(R.id.button_search_books);
-            rg = (RadioGroup) findViewById(R.id.radio_group_by);
+            rg = (RadioGroup) view.findViewById(R.id.radio_group_by);
             rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
             {
                 public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -45,8 +47,10 @@ public class BooksResult extends AppCompatActivity {
                     Toast.makeText(getBaseContext(), "changed to by "+radioResult, Toast.LENGTH_SHORT).show();
                 }
             });
+            ll.addView(view,0);
 
-        }else{
+
+        }else {
 
         }
 
